0072057b87a0ce12db6a99065d986a6cd18c7d44
Ustin4 <Mortyvp@yandex.by>
2024-05-09 21:57:42 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Alert} from "@mui/material";
+import {CityList, CountryInfo} from "./CityList/CityList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface City {
+    name: {
+        common: string;
+    };
 }
 
-export default App;
+export interface Country {
+    name: {
+        common: string;
+        official: string;
+    };
+    capital: string;
+    population: number;
+    continents: string[];
+    languages: {
+        [key: string]: string;
+    };
+    startOfWeek: string;
+    flags: {
+        png: string;
+    };
+}
+
+export function App() {
+    const [cities, setCities] = useState<City[]>([]);
+    const [selectedCountry, setSelectedCountry] = useState<City | null>(null);
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        axios
+            .get('https://restcountries.com/v3.1/all')
+            .then(response => {
+                setCities(response.data);
+            })
+            .catch(error => setError(error));
+    }, []);
+
+    const onClickHandler = (city: City) => {
+        setSelectedCountry(city);
+    };
+
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<CityList cities={cities} onClick={onClickHandler} />} />
+                <Route path="/country/:name" element={<CountryInfo />} />
+            </Routes>
+            {error && <Alert severity="error">{error.message}</Alert>}
+        </div>
+    );
+}
---
+++ src/CityList/CityList.tsx
@@ -0,0 +1,84 @@
+import {Alert, Divider, List, ListItem, ListItemText} from "@mui/material";
+import {useNavigate, useParams} from "react-router-dom";
+import {City, Country} from "../App";
+import {Fragment, useEffect, useState} from "react";
+import axios, {AxiosError} from "axios";
+import s from './list.module.css'
+interface CityListProps {
+    cities: City[];
+    onClick: (city: City) => void;
+}
+
+export function CityList({ cities, onClick }: CityListProps) {
+    const navigate = useNavigate();
+
+    const handleClick = (city: City) => {
+        onClick(city);
+        navigate(`/country/${city.name.common}`);
+    };
+
+    return (
+        <List className={s.list} aria-label="mailbox folders">
+            {cities.map((city: City) => (
+                <Fragment key={city.name.common}>
+                    <ListItem className={s.listItem}>
+                        <ListItemText onClick={() => handleClick(city)} primary={city.name.common} />
+                    </ListItem>
+                    <Divider component="li" />
+                </Fragment>
+            ))}
+        </List>
+    );
+}
+
+export function CountryInfo() {
+    const { name } = useParams();
+    const [selectedCountry, setSelectedCountry] = useState<Country | undefined>();
+    const [error, setError] = useState<AxiosError | undefined>();
+
+    useEffect(() => {
+        axios
+            .get(`https://restcountries.com/v3.1/name/${name}`)
+            .then(response => {
+                setSelectedCountry(response.data[0]);
+            })
+            .catch((error: AxiosError) => {
+                setError(error);
+            });
+    }, [name]);
+
+    if (!selectedCountry) return null;
+
+    return (
+        <>
+            <List className={s.list} aria-label="mailbox folders">
+                <ListItem>{name}</ListItem>
+                <ListItem>
+                    <ListItemText primary={`Official name: ${selectedCountry.name.official}`} />
+                </ListItem>
+                <ListItem>
+                    <ListItemText primary={`Capital: ${selectedCountry.capital}`} />
+                </ListItem>
+                <ListItem>
+                    <ListItemText primary={`Population: ${selectedCountry.population}`} />
+                </ListItem>
+                <ListItem>
+                    <ListItemText primary={`Continents: ${selectedCountry.continents}`} />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                        primary={`Languages: ${Object.values(selectedCountry.languages).join(", ")}`}
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText primary={`startOfWeek: ${selectedCountry.startOfWeek}`} />
+                </ListItem>
+                <ListItem>
+                    <img src={selectedCountry.flags.png} alt="" />
+                </ListItem>
+                <Divider component="li" />
+            </List>
+            {error && <Alert severity="error">{error.message}</Alert>}
+        </>
+    );
+}
